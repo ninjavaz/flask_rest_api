d643adc32be3148f53a2feb0bf98868ddbcd9990
ninjavaz <d1n22@onet.pl>
2021-05-23 14:02:58 +0200

    end of changes for a while

--- src/test.py
+++ src/test.py
@@ -1,9 +1,15 @@
 import requests
 
 BASE = "http://127.0.0.1:5000/"
+video_id = "1"
 
-response = requests.put(BASE + "video/2", {'name':'szybcy i wcsielki','likes': 10, 'views': 100})
+response = requests.put(BASE + "video/"+video_id, {'name':'szybcy i wcsielki','likes': 10, 'views': 100})
 print(response.json())
 
 input()
+response = requests.get(BASE + "video/"+video_id)
+print(response.json())
 
+
+
+# 36 minuta
