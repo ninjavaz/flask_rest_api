5876168565a101e93859b0328e73aa66aaaf90df
ninjavaz <d1n22@onet.pl>
2021-05-23 13:52:16 +0200

    more changes

--- src/test.py
+++ src/test.py
@@ -2,6 +2,8 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.post(BASE + "helloworld")
+response = requests.put(BASE + "video/2", {'name':'szybcy i wcsielki','likes': 10, 'views': 100})
 print(response.json())
+
+input()
 
